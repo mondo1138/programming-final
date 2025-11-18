9e3a8bce4ebba30910d17c39cb74b1f69bd5b2eb
tylerjalas231 <tylerjalas231@gmail.com>
2025-11-18 09:55:08 -0600

    Add files via upload
    Tyler's javascript

---
+++ JalasMemoryGame.js
@@ -0,0 +1,20 @@
+"use strict";
+let cards = [];
+function shuffleCards(cards)
+{
+for (let i = 0; i < cards.length; i++)
+    {
+        let randomCards = Math.floor(Math.random() * cards.length);
+    }
+}
+
+
+//start game function
+function startGame()
+{
+    incorrectAttempts = 0;
+    firstPick;
+    secondPick;
+    playing = false;
+    
+}
