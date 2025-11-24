2f401f4c353186e69d7f2ed71ed124761771d282
tylerjalas231 <tylerjalas231@gmail.com>
2025-11-23 20:19:38 -0600

    Update JalasMemoryGame.js

--- JalasMemoryGame.js
+++ JalasMemoryGame.js
@@ -13,8 +13,72 @@ for (let i = 0; i < cards.length; i++)
 function startGame()
 {
     incorrectAttempts = 0;
-    firstPick;
-    secondPick;
+    firstPick = null;
+    secondPick = null;
+    maxAttempts = 16;
     playing = false;
     
+    //still need to add more here
+}
+
+//handle card flip function
+function handleCardFlip()
+{
+    //handle the first pick
+    if (!firstPick)
+    {
+        for (let i = 0; i < cards.length; i++)
+        {
+            if (cards[i].isFlipped && !cards[i].isMatched)
+            {
+                firstPick = cards[i];
+                break;
+            }
+        }
+        return;
+    }
+    //handle the second pick
+    for (let j = 0; j < cards.length; j++)
+    {
+        if(cards[j].isFlipped && cards[j].isMatched && cards[j] !== firstPick)
+        {
+            secondPick = cards[j];
+            break;
+        }
+    }
+    if (!secondPick) return;
+    stopClicks = true;
+
+    //check for a match
+    if (firstPick.imageSource === secondPick.imageSource)
+    {
+        firstPick.matchCard();
+        secondPick.matchCard();
+
+        firstPick.imageElement.style.visibility = "hidden";
+        secondPick.imageElement.style.visibility = "hidden";
+
+        firstPick = null;
+        secondPick = null;
+        stopClicks = false;
+    }
+    else
+    {
+        incorrectAttempts++;
+
+        function setTimeout()
+        {
+            firstPick.hideCard();
+            secondPick.hideCard();
+
+            firstPick = null;
+            secondPick = null;
+            stopClicks = false;
+
+            if (incorrectAttempts >= maxAttempts)
+            {
+                alert("Game Over!! Too many attempts!");
+            }
+        }
+    }
 }
